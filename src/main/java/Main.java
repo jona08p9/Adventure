59cf11290b185fe690241b5e05fc5d8dce10b6e8
jona08p9 <jona08p9@stud.kea.dk>
2022-09-27 13:35:08 +0200

    adventure input fra bruger

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,38 @@
+import java.util.Scanner;
+
 public class Main {
 
 
-    //dette er en test
+    public static void main(String[] args) {
+        System.out.println("Write go north to go north");
+        System.out.println("Write go east to go east");
+        System.out.println("Write go south to go south");
+        System.out.println("Write go west go west");
+        Scanner scanner = new Scanner(System.in);
+        String north = "go north";
+        String east = "go east";
+        String south = "go south";
+        String west = "go west";
+        String direction;
+
+        direction = scanner.nextLine();
+
+        if (direction.equals(north)) {
+            System.out.println("Going north");
+        }
+        if (direction.equals(east)) {
+            System.out.println("Going east");
+        }
+        if (direction.equals(south)) {
+            System.out.println("Going south");
+        }
+        if (direction.equals(west)) {
+            System.out.println("Going west");
+        }
+
+    }
 }
+ //if (scanne == ){
+                    //  System.out.println(Going north
+
+
